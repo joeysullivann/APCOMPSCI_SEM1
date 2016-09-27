df555b662a8ac3cf2f6f0786d381e34199d245ee
joeysullivann <joeysull03@gmail.com>
2016-09-27 11:06:46 -0700

    Lab_01

--- Lab 01/ASCII.java
+++ Lab 01/ASCII.java
@@ -2,9 +2,9 @@ public class ASCII
 {
 	public static void main(String[]args)
 	{		
-		System.out.println("|\---/|");
+		System.out.println("|\\---/|");
 		System.out.println("| OvO |");
-		System.out.println(" \_^_/");
+		System.out.println(" \\_^_/");
 		
 		
 	}
