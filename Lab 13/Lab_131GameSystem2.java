24aa4f708860180f6f86662a2f021eb60484d52c
joeysullivann <joeysull03@gmail.com>
2017-02-15 11:18:39 -0800

    Lab_13.1

--- Lab 13/Lab_131GameSystem2.java
+++ Lab 13/Lab_131GameSystem2.java
@@ -1,16 +1,13 @@
-public class Lab_131GameSystem2
-{
-	public static void main(String[]args)
-	{
+public class Lab_131GameSystem2{
+	public static void main(String[]args){
 		Lab_131GameSystem objXBox = new Lab_131Xbox("XBox");
 		Lab_131GameSystem objPS = new Lab_131PS("PlayStation");
 		Lab_131GameSystem objPC = new Lab_131PC("PC");
-
 		System.out.println(objXBox);
-		System.out.println();
+		System.out.println("");
 		System.out.println(objPS);
-		System.out.println();
+		System.out.println("");
 		System.out.println(objPC);
-		System.out.println();
+		System.out.println("");
 	}
 }
