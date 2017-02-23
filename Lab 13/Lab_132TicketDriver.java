527ff102da19848cb1f76db92a78301bf0914630
joeysullivann <joeysull03@gmail.com>
2017-02-23 11:26:53 -0800

    Lab 13.2

--- Lab 13/Lab_132TicketDriver.java
+++ Lab 13/Lab_132TicketDriver.java
@@ -4,13 +4,13 @@ public class Lab_132TicketDriver{
 		Lab_132TicketSuperClass objAdvancedNoDiscount = new Lab_132AdvanceTicketClass(5);
 		Lab_132TicketSuperClass objStudentAdvanced = new Lab_132Student(15);
 		Lab_132TicketSuperClass objWalkup = new Lab_132Walkup();
-		System.out.println("Advance Ticket w/ Discount (15 days before)");
+		System.out.println("Advance Ticket with Discount (15 days before)");
 		System.out.println(objAdvancedDiscount);
 		System.out.println();
-		System.out.println("Advance Ticket w/o Discount (5 days before)");
+		System.out.println("Advance Ticket without Discount (5 days before)");
 		System.out.println(objAdvancedNoDiscount); 
 		System.out.println();
-		System.out.println("Student Advance Ticket w/ Discount");
+		System.out.println("Student Advance Ticket with Discount");
 		System.out.println(objStudentAdvanced);
 		System.out.println();
 		System.out.println("Walkup Ticket");
