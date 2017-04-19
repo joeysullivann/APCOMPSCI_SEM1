340e8ebaa1a4c8ac209dd2194099bb9604fa6de4
joeysullivann <joeysull03@gmail.com>
2017-04-19 11:35:06 -0700

    Lab 13

--- Lab 13/Lab_13ToyRunner.java
+++ Lab 13/Lab_13ToyRunner.java
@@ -2,7 +2,7 @@ public class Lab_13ToyRunner{
 	public static void main(String[]args){
 		Lab_13Store memes = new Lab_13Store("Hotwheel, Car, G.I.Joe, AF, PennyRacer, Car, Matchbox, Car, Star Wars, AF, Pullback, Car, Star Wars, AF");
 		System.out.println(memes);
-		System.out.println(memes.getMostFrequentToy());
-		System.out.println(memes.getMostFrequentType());
+		System.out.println("Most Frequent Toy:" + memes.getMostFrequentToy());
+		System.out.println("Most Frequent Type of Toy:" + memes.getMostFrequentType());
 	}
 }
